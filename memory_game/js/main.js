a444ae4fd6495aae32913b019befe6095696cfe6
jkarlin929 <osida11572@gmail.com>
2017-11-12 17:15:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,28 @@ console.log("Up and running!");
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardFour);
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -16,8 +37,10 @@ var checkForMatch = function () {
 
 
 var flipCard = function (cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].cardImage);
+  console.log("User flipped " + cards[cardId].suit);
   if (cardsInPlay.length === 2) {
   if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
@@ -30,3 +53,4 @@ checkForMatch();
 
 flipCard(0);
 flipCard(2);
+
